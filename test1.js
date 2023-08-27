f732a152557d31029134365d860d09352c27d4e3
ryanbomz <ryanbomz@gmail.com>
2023-08-27 09:44:13 +0700

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,1 +1,9 @@
 //test
+/*Clone repository
+- Git clone url-repo
+- Kalo uda buka di vs code
+- Bikin file baru , isi apa aja
+- Buka terminal:
+    - Git add .
+    - Git commit -m “init”
+    - Git push origin main*/
